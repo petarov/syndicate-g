ee4a136f4c68b226b36852c052132ad65ad5958c
Petar Petrov <petar.petrov.georgiev@gmail.com>
2013-12-17 22:22:09 +0100

    Added ejs dep. Basic http srv now operational

--- app/server.js
+++ app/server.js
@@ -1,3 +1,32 @@
 /**
-server.js
-*/
+ * server.js
+ */
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var _ = require('underscore')._;
+
+app.configure(function() {
+    // app.locals.pretty = true;
+    app.use(express.bodyParser());
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.logger());
+    app.engine('html', require('ejs').renderFile);
+});
+
+
+app.get('/', function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('hello world!');
+    // response.render('', {pretty: true});
+});
+
+server.listen(8080, function() {
+    console.log("It's on, borther.");
+});
+
+/**
+ * Return
+ */
+module.exports = app;
