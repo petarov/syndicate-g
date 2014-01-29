42f53c3a64058e0231c6b888ba415be294ecac05
Petar Petrov <petar.petrov.georgiev@gmail.com>
2014-01-29 22:20:39 +0100

    Updated config defaults

--- config-empty.js
+++ config-empty.js
@@ -12,8 +12,16 @@ var config = {
      * Server connection
      */
     server: {
-        url: '',
-        port: 8080
+        host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+        port: parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8080
+    },
+    /**
+     * Storage settings
+     */
+    db: {
+        redis_host: process.env.OPENSHIFT_REDIS_HOST || '127.0.0.1',
+        redis_port: process.env.OPENSHIFT_REDIS_PORT || '6379',
+        redis_secret: process.env.REDIS_PASSWORD || null
     },
     /**
      * GPlus settings
