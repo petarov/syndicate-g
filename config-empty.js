d3a7f0aa3e85a755546d8389df495546afe0f77e
Petar Petrov <petar.petrov@midpoints.de>
2016-09-04 22:49:41 +0200

    Split persistence modules and add db config

--- config-empty.js
+++ config-empty.js
@@ -1,41 +1,47 @@
 /*
- * syndicate-g
- * https://github.com/petarov/syndicate-g
- *
- * The MIT License (MIT)
- * Copyright (c) 2013 Petar Petrov
- */
+* syndicate-g
+* https://github.com/petarov/syndicate-g
+*
+* The MIT License (MIT)
+* Copyright (c) 2013 Petar Petrov
+*/
 
 var config = {
-    isDebug: false,
+  isDebug: false,
+  /**
+  * Server connection
+  */
+  server: {
+    host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+    port: parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8008
+  },
+  /**
+  * Storage settings
+  */
+  db: {
     /**
-     * Server connection
+     * Allowed types are 'redis' or 'memcache'.
+     * When using redis, make sure the redis configurations below are filled in.
      */
-    server: {
-        host: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
-        port: parseInt(process.env.OPENSHIFT_NODEJS_PORT) || 8008
-    },
-    /**
-     * Storage settings
-     */
-    db: {
-        redis_host: process.env.OPENSHIFT_REDIS_HOST || '127.0.0.1',
-        redis_port: process.env.OPENSHIFT_REDIS_PORT || '6379',
-        redis_secret: process.env.REDIS_PASSWORD || null
-    },
-    /**
-     * GPlus settings
-     */
-    gplus: {
-        apiKey: "",
-        maxResults: 10,
-    },
-    /**
-     * Feed generation options
-     */
-    rss: {
-        ttl: 60
-    }    
+    type: 'redis',
+
+    redis_host: process.env.OPENSHIFT_REDIS_HOST || '127.0.0.1',
+    redis_port: process.env.OPENSHIFT_REDIS_PORT || '6379',
+    redis_secret: process.env.REDIS_PASSWORD || null
+  },
+  /**
+  * GPlus settings
+  */
+  gplus: {
+    apiKey: '',
+    maxResults: 10,
+  },
+  /**
+  * Feed generation options
+  */
+  rss: {
+    ttl: 60
+  }
 };
 
 module.exports = config;
